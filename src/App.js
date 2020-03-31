d2cd7aa8f34daeef55217286eae277676fbfa4f7
Victor <Victor.Gamble@outlook.com>
2020-03-31 11:31:55 -0400

    removed the IssueDetails from inside of the App Function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import IssueList from './components/IssueList'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import IssueDetails from './components/IssueDetails'
+import {Container} from 'bloomer'
 import 'bulma/css/bulma.css'
 
 
 function App() {
   return (
     <div className="App">
-      <IssueList />
+     <Container>
+     <Router>
+       <Route path ="/" exact component={IssueList}/>
+       <Route path="/issue/:issue_number?" component={IssueDetails}/>
+     </Router>
+     </Container>
     </div>
   );
 }

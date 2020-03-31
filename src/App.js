25322474857a989453ea2b0a46fa03f614bc2b6a
Victor <Victor.Gamble@outlook.com>
2020-03-31 10:42:35 -0400

    imported react-roter! created routes to link to home page and issue page by issue number

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ import 'bulma/css/bulma.css'
 function App() {
   return (
     <div className="App">
-     <Router>
-       <Route path ="/" exact component={IssueList}/>
-       <Route path="/issue/:issue_number?" component={IssueDetails}/>
-     </Router>
+      <IssueList />
     </div>
   );
 }

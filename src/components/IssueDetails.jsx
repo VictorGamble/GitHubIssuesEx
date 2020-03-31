a14bcf64da2ca4efa4c4d8c4f829dbbfd863c2de
Victor <Victor.Gamble@outlook.com>
2020-03-31 13:53:39 -0400

    added elements that will show the issue title and body.

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -6,11 +6,9 @@ export default class IssueDetails extends Component{
         issueDetails: []
     }
 
-  dataLoad = async () => {
+  dataLoad = async (url) => {
     try {
-      const response = await fetch(
-        `https://api.github.com/repos/facebook/create-react-app/issues`
-      );
+      const response = await fetch(url);
       const data = await response.json();
       return data;
     } catch (error) {
@@ -20,30 +18,28 @@ export default class IssueDetails extends Component{
   };
 
     async componentDidMount(){ 
-       console.log("Props are =>", this.props)
         try {
             const {issue_number} = this.props.match.params;
-            console.log("issue number is =>", issue_number);
             const issueDetails = await this.dataLoad(`https://api.github.com/repos/facebook/create-react-app/issues/${issue_number}`)
-            console.log("IssueDetails1 are =>", issueDetails)
-
-        this.setState({
-         issueDetails: issueDetails
-        })
+        
+            this.setState({
+                issueDetails: issueDetails
+            })
         } catch (error) {
             console.error("Error", error)
             return error
         }
     }
+
     render(){
         const {issueDetails} = this.state
-        console.log("issueDetails2 are =>", issueDetails)
+        console.log("issueDetails are =>", issueDetails)
         return(
             <div>
-                <h1>
+                <Title>
                     {issueDetails.title}
-                </h1>
-            <p>{issueDetails.body}</p>
+                </Title>
+        <p>{issueDetails.body}</p>
             </div>
         )
     }
